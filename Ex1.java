cf377f076fe94a9c107688df7d645f6be6745a34
Luigi Vicchietti <73287535+LuigiVicchietti@users.noreply.github.com>
2020-12-10 20:17:04 -0300

    Add files via upload

--- Ex1.java
+++ Ex1.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
 public class Ex1 {
 	public static void main(String[] args) {
-		int r;
-		for (int n=1; n<11; n++) {
-		System.out.println("Tabuada do número "+n+":");
-		for(int i=1; i<11; i++) {
-			r = n*i;
-			System.out.println(n+" x "+i+" = "+r);
+		Scanner in = new Scanner(System.in);
+		final int TAM = 20;
+		int a[], i, contpar=0, contimpar=0;
+		a = new int[TAM];
+	
+		for(i =0; i<TAM; i++) {
+			System.out.println("Entre com o "+(i+1)+ "º número:");
+			a[i] = in.nextInt();
+			if (a[i]%2 == 0) {
+				contpar++;
+			}else {
+				contimpar++;
+			}
 		}
+		System.out.println("Números inseridos:");
+		for(i =0; i<TAM; i++) {
+			System.out.print(a[i]+"  ");
 		}
-	}
+		System.out.println("\nSão pares: "+contpar);
+		System.out.println("São ímpares: "+contimpar);
+		}
+	
 }
--- Ex3.java
+++ Ex3.java
@@ -2,49 +2,28 @@ import java.util.Scanner;
 public class Ex3 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner (System.in);
-		int id, qtp=0, qps=0;
-		double h, qh=0, sh=0, ps, maior=0, menor=99;
-		for (int p=1; p<26; p++) {
-			System.out.println("Digite a idade da "+p+"º pessoa.");
-			id = in.nextInt();
-			System.out.println("Digite a altura da "+p+"º pessoa.");
-			h = in.nextDouble();
-			System.out.println("Digite o peso da "+p+"º pessoa.");
-			ps = in.nextDouble();
-			if (id>50) {
-				qtp++;
+		final int TAM = 10;
+		int a[], i, j, restozero=0;
+		a = new int [TAM];
+		
+		for(j=0; j<TAM; j++) {
+			System.out.println("Entre com o " + (j+1) + "º número");
+			a[j] = in.nextInt();
+			
+			for(i=1; i<=a[j]; i++) {
+				if (a[j]%i==0) {
+					restozero++;
+				}
 			}
-			if ((id>10) && (id<20)) {
-				qh++;
-				sh = h + sh;
+			if (a[j]==1) {
+				System.out.println(a[j] + " não é primo");
+			}else if (restozero<=2) {
+				System.out.println(a[j] + " é primo");
+			}else {
+				System.out.println(a[j] + " não é primo");
 			}
-			if (ps<40) {
-				qps++;
-			}
-			if (id>maior) {
-				maior = ps;
-			}else if (id<menor){
-				menor = ps;
-			}
+			restozero = 0;
 		}
-		
-		if (qtp>0) {
-		System.out.println("Quantidade de pessoas com idade superior a 50 anos: "+qtp);
-		}else {
-			System.out.println("Quantidade de pessoas com idade superior a 50 anos: Nenhuma");
 		}
-		
-		if (qh>0) {
-		System.out.println("Média das alturas das pessoas com idade entre 10 a 20 anos: "+sh/qh+" m");
-		}else {
-			System.out.println("Média das alturas das pessoas com idade entre 10 a 20 anos: Não possui");
-		}
-		
-		System.out.println("Porcentagem de pessoas com peso inferior a 40 quilos: "+(qps*100)/25+"%");
-		
-		System.out.println("Peso da pessoa mais velha: "+maior+" kg");
-		System.out.println("Peso da pessoa mais nova: "+menor+" kg");
-		
-		in.close();
+	
 	}
-}
